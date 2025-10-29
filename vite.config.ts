e5c14b17545f45583d030395914263977944e8a1
kishansoni2726 <43043137+kishansoni2726@users.noreply.github.com>
2025-10-30 01:19:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ use relative paths for assets when deployed
 })
